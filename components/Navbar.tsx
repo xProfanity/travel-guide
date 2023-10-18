a6168ecd08ca5d211d80eb81c5a39dc2631a9945
got-crypto <pecpalmplay@gmail.com>
2023-10-18 14:27:18 +0200

    made navbar layout

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,3 +1,4 @@
+import { NAV_LINKS } from "@/constants";
 import Image from "next/image";
 import Link from "next/link";
 
@@ -12,6 +13,18 @@ export default function Navbar() {
                 height={29}
             />
         </Link>
+
+        <ul className="hidden h-full gap-12 lg:flex">
+            {NAV_LINKS.map((link) => (
+                <Link href={link.href} key={link.key} className="regular-16 text-gray-50 flexCenter cursor-pointer pb-1.5 transition-all hover:font-bold">
+                    {link.label}
+                </Link>
+            ))}
+        </ul>
+
+        <div className="lg:flexCenter hidden ">
+            <button type="button">click me</button>
+        </div>
     </div>
   )
 }
