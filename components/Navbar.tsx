fc4ca337737fb5c3666551dce1d1a3305a5af9db
got-crypto <pecpalmplay@gmail.com>
2023-10-18 13:34:37 +0200

    added public folder assets

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,6 +1,8 @@
 
 export default function Navbar() {
   return (
-    <div>Navbar</div>
+    <div className="border-2 border-red-500 flexBetween max-container padding-container relative z-30 py-5">
+
+    </div>
   )
 }
