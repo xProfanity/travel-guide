26cf590c2a869aa3dd89cea60edddb13819eb1d3
got-crypto <pecpalmplay@gmail.com>
2023-10-20 22:50:51 +0200

    finished guide section

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -21,6 +21,40 @@ export default function Guide() {
           </p>
         </div>
       </div>
+
+      <div className="flexCenter max-container relative ">
+        <Image
+          src={"/boat.png"}
+          alt="boat"
+          width={1440}
+          height={580}
+          className="w-full object-cover object-center xl:rounded-3xl 2xl:rounded-5xl"
+        />
+
+        <div className="absolute flex bg-white py-8 pl-5 pr-7 rounded-3xl border shadow-md md:left-[5%] lg:top-20">
+          <Image
+            src={"/meter.svg"}
+            alt="meter"
+            width={16}
+            height={158}
+            className="h-full w-auto"
+          />
+          <div className="flexBetween flex-col">
+            <div className="flex w-full flex-col">
+              <div className="flexBetween w-full">
+                <p className="regular-16 text-gray-50">Destination</p>
+                <p className="bold-16 text-green-50">48 min</p>
+              </div>
+              <p className="bold-20 mt-2">Aguas Calientes</p>
+            </div>
+
+            <div className="flex w-full flex-col">
+              <p className="regular-16 text-gray-50">Start track</p>
+              <h4 className="bold-20 mt-20 whitespace-nowrap">Wonorejo Pasuruan</h4>
+            </div>
+          </div>
+        </div>
+      </div>
     </section>
   )
 }
