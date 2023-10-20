333e41ee7951955bd00d5bcb4d6ff35c512ca3bf
got-crypto <pecpalmplay@gmail.com>
2023-10-20 22:58:13 +0200

    added features background

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,6 +1,19 @@
+import Image from "next/image";
 
 export default function Features() {
   return (
-    <div>Features</div>
+    <section className="flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
+      <div className="max-container padding-container relative w-full justify-end">
+        <div className="flex flex-1 lg:min-h-[900px]">
+          <Image
+            src={"/phone.png"}
+            alt="phone"
+            width={440}
+            height={1000}
+            className="feature-phone"
+          />
+        </div>
+      </div>
+    </section>
   )
 }
