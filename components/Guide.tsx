a2a61d1d3a3012b614d79f8c95d1ba58d02469c7
got-crypto <pecpalmplay@gmail.com>
2023-10-20 18:17:34 +0200

    created guide header

--- components/Guide.tsx
+++ components/Guide.tsx
@@ -1,6 +1,26 @@
+import Image from "next/image";
 
 export default function Guide() {
   return (
-    <div>Guide</div>
+    <section className="flexCenter flex-col">
+      <div className="padding-container max-container w-full pb-24">
+        <Image
+          src={"/camp.svg"}
+          alt="camp"
+          width={50}
+          height={50}
+        />
+        <p className="uppercase regular-18 -mt-1 mb-3 text-green-50">
+          We are here for you
+        </p>
+        <div className="flex flex-wrap justify-between gap-5 lg:gap-10">
+          <h2>Guide You to Easy Path</h2>
+          <p>
+            Only with the hilink application you will no longer get lost and get lost again, because we already support offline maps when there is no internet connection in the field.
+            Invite your friends, relatives and friends to have fun in the wilderness through the valley and reach the top of the mountain
+          </p>
+        </div>
+      </div>
+    </section>
   )
 }
