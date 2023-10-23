66140aabbd6fac26dab5f063db2df39b85cbd8e5
got-crypto <pecpalmplay@gmail.com>
2023-10-24 01:51:10 +0200

    created featureitem components

--- components/Features.tsx
+++ components/Features.tsx
@@ -1,9 +1,10 @@
+import { FEATURES } from "@/constants";
 import Image from "next/image";
 
 export default function Features() {
   return (
     <section className="flex-col flexCenter overflow-hidden bg-feature-bg bg-center bg-no-repeat py-24">
-      <div className="max-container padding-container relative w-full justify-end">
+      <div className="max-container padding-container relative w-full flex justify-end">
         <div className="flex flex-1 lg:min-h-[900px]">
           <Image
             src={"/phone.png"}
@@ -25,8 +26,43 @@ export default function Features() {
             />
             <h2 className="bold-40 lg:bold-64">Our Features</h2>
           </div>
+          <ul className="mt-10 grid gap-10 md:grid-cols-2 lg:mt-20 lg:gap-20">
+            {FEATURES.map((feature) => (
+              <FeatureItem
+                key={feature.title} 
+                title={feature.title}
+                description={feature.description}
+                icon={feature.icon}
+                variant={feature.variant}
+              />
+            ))}
+          </ul>
         </div>
       </div>
     </section>
   )
 }
+
+interface FeatureProps {
+  title: string;
+  icon: string;
+  variant: string;
+  description: string;
+}
+
+const FeatureItem = ({title, icon, variant, description}: FeatureProps) => (
+  <li className="flex w-full flex-1 flex-col items-start">
+    <div className={`rounded-full p-4 lg:p-7 bg-green-50`}>
+      <Image
+        src={icon}
+        alt="map"
+        width={28}
+        height={28}
+      />
+    </div>
+    <h2 className="bold-20 lg:bold-32 mt-5 capitalize">
+      {title}
+    </h2>
+    <p className="regular-16 mt-5 bg-white/80 text-gray-30 lg:mt-[30px] lg:bg-none">{description}</p>
+  </li>
+)
