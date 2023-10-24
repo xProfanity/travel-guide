59dd2035e1c0453ad77f92b5f259b51a1002810b
got-crypto <pecpalmplay@gmail.com>
2023-10-24 20:18:22 +0200

    created get app section

--- components/Button.tsx
+++ components/Button.tsx
@@ -5,17 +5,18 @@ type ButtonProps = {
     title: string;
     icon?: string;
     variant: string;
+    full?: boolean
 }
 
 
-export default function Button({type, title, icon, variant}: ButtonProps) {
+export default function Button({type, title, icon, variant, full}: ButtonProps) {
   return (
     <button
         type={type}
-        className={`flexCenter gap-3 rounded-full border ${variant}`}
+        className={`flexCenter gap-3 rounded-full border ${full && 'w-full'} ${variant} `}
     >
         {icon && <Image src={icon} alt={title} width={24} height={24} />}
-        <label className="bold-16 whitespace-nowrap">{title}</label>
+        <label className="bold-16 whitespace-nowrap cursor-pointer">{title}</label>
     </button>
   )
 }
